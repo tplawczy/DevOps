c5dcd9306e52dd2e0c0c49280999830cac75ad91
tplawczyk <tomasz.plawczyk@op.pl>
2021-03-23 12:51:27 +0100

    comment retry_strategy line

--- mybackend/index.js
+++ mybackend/index.js
@@ -4,18 +4,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 const redis = require('redis');
 
 const redisClient = redis.createClient({
         host: "myredis",
         port: 6379,
-        retry_strategy: () => 1000
+        //retry_strategy: () => 1000
 });
 app.get("/",(req, res) => {
     res.send("Hello Word!");
     
 });
-const PORT=5000;
+const PORT = 5000;
 app.listen(PORT, ()=>{
-    console.log('API listening on port ${PORT}');
+    console.log(`API listening on port ${PORT}`);
 });
